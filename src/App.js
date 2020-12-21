53ed49b5eab6f617258335404c7618164f4da8fb
Snehit <snehitchats@gmail.com>
2020-12-21 13:55:20 +0530

    App setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 import { Entry } from './pages/entry/Entry.page'
+import { DefaultLayout } from './layout/DefaultLayout'
+import { Dashboard } from "./pages/dashboard/Dashboard.page"
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
+      {/* in the above case the sample text is stored in {props.children} of default layout */}
     </div>
   );
 }
